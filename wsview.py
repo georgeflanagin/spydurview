dfc383ced018889e48248fd3a4fdb3f76bc31a05
George <me@georgeflanagin.com>
2023-08-07 14:08:22 -0400

    Bug fixes.

--- wsview.py
+++ wsview.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+"""
+    This program will query a list of Linux computers for status
+    of memory and CPU usage. 
+"""
+
+
 import typing
 from   typing import *
 
@@ -27,6 +33,7 @@ import pickle
 ###
 # From hpclib
 ###
+from   dorunrun import dorunrun
 import fileutils
 import linuxutils
 from   sloppytree import SloppyTree
@@ -61,10 +68,14 @@ def append_pickle(o:object, file_name:str) -> int:
     Append a pickle of o to file_name.
     """
     global logger
+    logger.info(piddly("append_pickle"))
     with open(file_name, 'ab+') as f:
+        logger.info(piddly("Pickle file open, waiting for lock."))
         fcntl.lockf(f, fcntl.LOCK_EX)
+        logger.info(piddly("locked."))
         try:
             pickle.dump(o, f)
+            logger.info(piddly("Pickle written."))
         except pickle.PicklingError as e:
             logger.error(piddly(f"{e}"))
         finally:
@@ -77,10 +88,11 @@ def collectdata(host:str) -> int:
     This is the function executed by the child process.
     """
     global logger, DAT_FILE
+    logger.info(piddly(f"Collecting data about {host}"))
     try:
-        append_pickle((host, 'stats', get_actual_stats(host)), DAT_FILE)
-        append_pickle((host, 'mem', get_actual_mem_usage(host)), DAT_FILE)
-        append_pickle((host, 'cores', get_actual_core_usage(host)), DAT_FILE)
+        append_pickle([host, 'stats', get_actual_stats(host)], DAT_FILE)
+        append_pickle([host, 'mem', get_actual_mem_usage(host)], DAT_FILE)
+        append_pickle([host, 'cores', get_actual_cores_usage(host)], DAT_FILE)
 
     except Exception as e:
         logger.error(piddly(f"{e}"))
@@ -102,6 +114,8 @@ def get_actual_cores_usage(node:str) -> SloppyTree:
     """
     ssh to the node and get the number of cores that is actually used.
     """
+    global logger
+    logger.info(piddly(f"get_actual_cores_usage for {node}"))
     cmd = "ssh -o ConnectTimeout={} {} 'cat /proc/loadavg'"
     return parse_loadavg(dorunrun(cmd.format(myargs.wait_sec, node), return_datatype = str))
 
@@ -110,7 +124,9 @@ def get_actual_mem_usage(node:str) -> SloppyTree:
     """
     ssh to the node and calculate how much memory is used.
     """
-    cmd = "ssh -o ConnectTimeout={} {} 'head -2 cat /proc/meminfo'"
+    global logger
+    logger.info(piddly(f"get_actual_mem_usage for {node}"))
+    cmd = "ssh -o ConnectTimeout={} {} 'cat /proc/meminfo | head -3'"
     return parse_meminfo(dorunrun(cmd.format(myargs.wait_sec, node), return_datatype = str))
    
 @trap
@@ -118,6 +134,8 @@ def get_actual_stats(node:str) -> SloppyTree:
     """
     ssh to the node and retrive mpstat info.
     """
+    global logger
+    logger.info(piddly(f"get_actual_stats for {node}"))
     cmd = "ssh -o ConnectTimeout={} {} 'mpstat | tail -2'"
     return parse_mpstat(dorunrun(cmd.format(myargs.wait_sec, node), return_datatype=str))
     
@@ -127,8 +145,9 @@ def get_hostnames() -> tuple:
     Gets the current list of workstations.
     """
     global myargs, logger
+    logger.info("get_hostnames")
     try:
-        hosts = tuple(fileutils.read_whitespace_file(myargs.input))
+        hosts = tuple(_ for _ in fileutils.read_whitespace_file(myargs.input))
     except Exception as e:
         logger.error(f"Could not use {myargs.input} because {e}")
         sys.exit(os.EX_DATAERR)
@@ -136,6 +155,9 @@ def get_hostnames() -> tuple:
     if not len(hosts):
         logger.error(f"No hosts found.")
         sys.exit(os.EX_DATAERR)
+
+    return hosts
+
     
 @trap
 def get_info() -> SloppyTree:
@@ -145,8 +167,8 @@ def get_info() -> SloppyTree:
     global logger, DAT_FILE
     logger.info(piddly("Entered get_info"))
 
-    t = SloppyTree(dict.fromkeys(get_hostnames(), None))
-    fork_ssh(t.keys())
+    t = SloppyTree()
+    fork_ssh(get_hostnames())
 
     for p in extract_pickle(DAT_FILE):
         host, data_type, data_tree = p
@@ -159,10 +181,14 @@ def parse_meminfo(s:str) -> SloppyTree:
     """
     Parse /proc/meminfo and return keys and values.
     """
+    global logger
     d = {}
     for line in s.split('\n'):
         label, value, _ = line.split()
-        d[label[:-1].lower()] = int(value)
+        try:
+            d[label[:-1].lower()] = int(value)
+        except Exception as e:
+            d[label[:-1].lower()] = -1729
 
     return d
 
@@ -174,7 +200,7 @@ def parse_loadavg(s:str) -> SloppyTree:
     """
     d = SloppyTree()
 
-    d.minute_1, d.minute_5, d.minute_15, threads = s.split()
+    d.minute_1, d.minute_5, d.minute_15, threads, _ = s.split()
     d.running_threads, d.total_threads = threads.split('/')
     for k, v in d.items():
         d[k] = float(v)
@@ -203,7 +229,7 @@ def fork_ssh(list_of_nodes:tuple) -> None:
     Multiprocesses to ssh to each node and retrieve this information 
     in parallel, significantly speeding up the process.
     '''
-    global DAT_FILE, logger
+    global logger, myargs, DAT_FILE
     logger.info("Entered fork_ssh")
     
     try:
@@ -225,7 +251,9 @@ def fork_ssh(list_of_nodes:tuple) -> None:
         if (pid := os.fork()):
             pids.add(pid)
             continue
+
         try:
+            exit_code = 0
             exit_code = collectdata(node)
         finally:
             os._exit(exit_code)
@@ -247,7 +275,8 @@ def wsview_main() -> int:
     logger.info(piddly("Entered wsview_main"))
     
 
-    pprint(get_info())
+    data = dict(get_info())
+    print(data)
 
     return os.EX_OK
 
@@ -256,23 +285,23 @@ if __name__ == '__main__':
     
     this_prog = os.path.basename(__file__)[:-3]
     logfile = f"{this_prog}.log"
+    default_host_file = os.path.join(os.getcwd(), 'hosts')
 
     parser = argparse.ArgumentParser(prog="wsview", 
         description="What wsview does, wsview does best.")
 
+    parser.add_argument('-c', '--columns', type=str,
+        default="")
+    parser.add_argument('-i', '--input', type=str, default=default_host_file,
+        help=f"A whitespace delimited file of [user@]host names. Default file is {default_host_file}")
+    parser.add_argument('-o', '--output', type=str, default="",
+        help="Output file name. Defaults to stdout.")
+    parser.add_argument('-w', '--wait-sec', type=int, default=1,
+        help="Maximum seconds to wait for a response. The default is 1.")
+    parser.add_argument('-v', '--verbose', type=int, default=logging.DEBUG, 
+        help=f"Sets the loglevel. Values between {logging.NOTSET} and {logging.CRITICAL}. Default is {logging.DEBUG}")
     parser.add_argument('-z', '--zap-logfile', action='store_true',
         help="Remove old logfile if present.")
-    parser.add_argument('-r', '--refresh', type=int, default=60, 
-        help="Refresh interval defaults to 60 seconds. Set to 0 to only run once.")
-    parser.add_argument('-i', '--input', type=str, default=os.path.join(os.getcwd(),'hosts'),
-        help="If present, --input is interpreted to be a whitespace delimited file of host names.")
-    parser.add_argument('-o', '--output', type=str, default="",
-        help="Output file name")
-    parser.add_argument('-w', '--wait-sec', type=int, default=1,
-        help="Number of seconds to wait for a response.")
-    parser.add_argument('-v', '--verbose', type=int, default=logging.DEBUG, 
-        help=f"Sets the loglevel. Values between {logging.NOTSET} and {logging.CRITICAL}.")
-
 
     myargs = parser.parse_args()
     verbose = ( myargs.verbose 
